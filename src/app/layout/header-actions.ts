ef561e5d9be56da3b8f88cf3f5d8db5ac9212b7a
Dan Hutchinson <Dan.Hutchinson+cai@coxautoinc.com>
2025-11-12 22:41:19 +0000

    refactor: remove unused MatButton import from header-actions component

--- src/app/layout/header-actions.ts
+++ src/app/layout/header-actions.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
