4d79c461990f7cb3f0da2e6b60afd96900e0185d
Dan Hutchinson <danielhutchinson5@gmail.com>
2025-11-13 16:15:37 +0000

    Refactor vehicle card and remove VehicleService
    Updated vehicle card to use Angular Material and display an image. Removed VehicleService and related imports, simplifying vehicle search page. Added car_6.png image for vehicle card display.

--- src/app/layout/vehicle-card/vehicle-card.ts
+++ src/app/layout/vehicle-card/vehicle-card.ts
@@ -1,12 +1,32 @@
 import { Component, input } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { VehicleGroup } from '../../types/vehicle';
 
 @Component({
   selector: 'app-vehicle-card',
-  imports: [],
+  imports: [MatCard, MatCardContent, MatCardHeader, MatCardTitle],
   template: `
-    <h3 class="text-lg text-bold">{{ vehicle().specification.model }}</h3>
-    <h4 class="text-md mb-4">{{ vehicle().specification.derivative }}</h4>
+    <mat-card class="max-w-sm mb-6">
+      <div class="overflow-hidden">
+        <img
+          src="/img/car_6.png"
+          [alt]="vehicle().specification.manufacturer + ' ' + vehicle().specification.model"
+          class="w-full h-full object-cover"
+        />
+      </div>
+
+      <mat-card-header>
+        <mat-card-title>
+          {{ vehicle().specification.manufacturer }} {{ vehicle().specification.model }}
+        </mat-card-title>
+      </mat-card-header>
+
+      <mat-card-content>
+        <h3 class="text-xs text-gray-400">
+          {{ vehicle().specification.derivative }}
+        </h3>
+      </mat-card-content>
+    </mat-card>
   `,
   styles: ``,
 })
--- src/app/pages/vehicle-search/vehicle-search.ts
+++ src/app/pages/vehicle-search/vehicle-search.ts
@@ -1,26 +1,16 @@
 import { Component, inject, signal, Signal } from '@angular/core';
-import { JsonPipe } from '@angular/common';
-import { toSignal } from '@angular/core/rxjs-interop';
 import { SearchBanner } from './search-banner';
-import { VehicleGroup } from '../../types/vehicle';
-import { VehicleService } from '../../services/vehicle';
 import { StorefrontStore } from '../../storefront-store';
 import { VehicleCard } from '../../layout/vehicle-card/vehicle-card';
 @Component({
   selector: 'app-vehicle-search',
-  imports: [SearchBanner, JsonPipe, VehicleCard],
+  imports: [SearchBanner, VehicleCard],
   template: `
     <app-search-banner></app-search-banner>
 
     <main class="container mx-auto py-4" role="main">
       <section aria-labelledby="content-heading">
         <h2 class="sr-only">Vehicle Search Results</h2>
-        <p role="text">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi vero reprehenderit libero
-          rem quis provident velit omnis sed aperiam ducimus debitis soluta iusto odit a, quasi
-          harum nisi tempora esse.
-        </p>
-
         <section>
           @for (vehicle of vehicles(); track vehicle.vehicleId) {
           <app-vehicle-card [vehicle]="vehicle"></app-vehicle-card>
